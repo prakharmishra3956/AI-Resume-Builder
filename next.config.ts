f143d63b3d19f686f71d96bb6db4bc83236b0ed6
prakharmishra3956 <mishrayash638678@gmail.com>
2025-09-12 16:05:27 +0530

    Revert next.config.ts to disable LightningCSS and fallback to PostCSS

--- next.config.ts
+++ next.config.ts
@@ -7,22 +7,8 @@ const nextConfig: NextConfig = {
     },
     // disable LightningCSS on Vercel (fallback to PostCSS)
     optimizeCss: false,
-  },
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "w0mlmrgwbziwquaq.public.blob.vercel-storage.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "6m0cxj8fgdmf3ibh.public.blob.vercel-storage.com",
-        pathname: "/**",
-      },
-    ],
-  },
+  }
 };
+
 
 export default nextConfig;
